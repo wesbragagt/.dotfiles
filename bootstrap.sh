5bed13c0aaa1322bc454c1e70c4472468615e26a
Weslley Braga <wesbragagt@gmail.com>
2021-08-31 23:08:34 -0500

    sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 # This script will symlink all the necessary files to the home directory
 
 set -e
-
-# roots
-nvim_root="./nvim/init.vim"
-zsh_root="./zsh/.zshrc"
-tmux_root="./tmux/.tmux.conf"
-
-# links
-nvim_link="$HOME/.config/nvim/init.vim"
-zsh_link="$HOME/.zshrc"
-tmux_link="$HOME/.tmux.conf"
-
 # Run
-ln -s -f nvim_root nvim_link &&
-ln -s -f zsh_root zsh_link &&
-ln -s -f tmux_root tmux_link
+sudo ln -svf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim &&
+sudo ln -svf ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json &&
+sudo ln -svf ~/.dotfiles/zsh/.zshrc ~/.zshrc &&
+sudo ln -svf ~/.dotfiles/tmux/.tmux.config ~/.tmux.config
 
 echo "Done symlinking files."
