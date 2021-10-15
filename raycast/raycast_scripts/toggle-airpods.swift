1994f96e84e2883c36277259f577c9bc8542e880
weslley.delimabragas <weslley.delimabragasilva@asurion.com>
2021-10-15 10:53:28 -0500

    added airpods script

--- raycast/raycast_scripts/toggle-airpods.swift
+++ raycast/raycast_scripts/toggle-airpods.swift
@@ -1,5 +1,17 @@
 #!/usr/bin/swift
-
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Toggle AirPods
+// @raycast.mode silent
+// @raycast.packageName Audio
+//
+// Optional parameters:
+// @raycast.icon images/airpod.png
+//
+// Documentation:
+// @raycast.description Toggle AirPods bluetooth device
+// @raycast.author Nichlas W. Andersen
+// @raycast.authorURL https://github.com/itsnwa
 import IOBluetooth
 
 // Get your device's MAC address by option (⌥) + clicking the bluetooth icon in the menu bar
