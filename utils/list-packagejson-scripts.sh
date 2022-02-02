d01c6a9090ac7efc0cc0a33fa740b0fa8cc6d734
weslleybraga <weslley@bambee.com>
2022-02-02 16:04:56 -0600

    updated fzf package.json list

--- utils/list-packagejson-scripts.sh
+++ utils/list-packagejson-scripts.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
-
+set -e
 # Pipes a list of scripts available in package.json to fzf for selection
-
 if [ ! -e package.json ]; then
   echo "Not a package.json in this directory"
   exit -1
 else
-
-  yarn_command="npm run $(jq '.scripts | keys[]' package.json | sed 's/"//g' | fzf
-)"
-
-  eval $yarn_command
+  run_command="npm run $(jq '.scripts | keys[]' package.json | sed 's/"//g' | fzf)"
+  eval $run_command
 fi
 
