c66f0a2383a3c21b59c35d495ff22d4ca36b6f66
Weslley Braga <wesbragagt@gmail.com>
2022-05-17 14:00:17 -0500

    refactor performance lua file

---
+++ nvim/.config/nvim/lua/wesbragagt/performance.lua
@@ -0,0 +1,17 @@
+local status_ok, filetype = pcall(require, "filetype")
+if not status_ok then
+  return
+end
+
+vim.g.did_load_filetypes = 1
+filetype.setup {
+  overrides = {
+    extensions = {
+      tf = "terraform",
+      tfvars = "terraform",
+      tfstate = "json"
+    }
+  }
+}
+-- Speed up neovim startup
+require("impatient")
