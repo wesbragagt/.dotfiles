ec9267fed5615c9f23efa14312ad3fa5a869f19d
Weslley Braga <wesbragagt@gmail.com>
2022-07-04 00:49:59 -0500

    using volar for vuejs

--- nvim/.config/nvim/lua/wesbragagt/_lsp.lua
+++ nvim/.config/nvim/lua/wesbragagt/_lsp.lua
@@ -208,48 +208,9 @@ setup_server(
 )
 
 setup_server(
-	"vuels",
+	"volar",
 	config({
-		cmd = { "vls" },
 		filetypes = { "vue" },
-		init_options = {
-			config = {
-				css = {},
-				emmet = {},
-				html = {
-					suggest = {},
-				},
-				javascript = {
-					format = {},
-				},
-				stylusSupremacy = {},
-				typescript = {
-					format = {},
-				},
-				vetur = {
-					completion = {
-						autoImport = true,
-						tagCasing = "kebab",
-						useScaffoldSnippets = false,
-					},
-					format = {
-						defaultFormatter = {
-							js = "prettier",
-							ts = "prettier",
-						},
-						defaultFormatterOptions = {},
-						scriptInitialIndent = false,
-						styleInitialIndent = false,
-					},
-					useWorkspaceDependencies = true,
-					validation = {
-						script = true,
-						style = true,
-						template = true,
-					},
-				},
-			},
-		},
 	})
 )
 
