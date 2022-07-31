0951a221804fd8780b771b9f194b8866cec6ec73
Weslley Braga <wesbragagt@gmail.com>
2022-07-31 16:19:16 -0500

    tmux conf new session creator

--- utils/tmux-new-session.sh
+++ utils/tmux-new-session.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-if [[ -z $WORK_DIR ]];then
+work_dir=$1
+
+if [[ -z $work_dir ]];then
   echo "please set a WORK_DIR for this script to work"
   exit 1;
-fi
-
-# lists directories within a work_dir and creates a new tmux session for that directory
-directory=$(ls -d `printf "$WORK_DIR/*"` | fzf --reverse --header create-session)
-echo $WORK_DIR
-
-if [[ -z $directory ]];then
-  exit 0;
 else
-  session_name=$(basename $directory)
-  # does not matter if session is created just switch to it
-  tmux new-session -d -s $session_name -c $directory &> /dev/null
-  tmux switch-client -t $session_name 
+  directory=$(ls -d `printf "$work_dir/*"` | fzf --reverse --header create-session)
+  if [[ -z $directory ]];then
+    exit 0;
+  else
+    session_name=$(basename $directory)
+    # does not matter if session is created just switch to it
+    tmux new-session -d -s $session_name -c $directory &> /dev/null
+    tmux switch-client -t $session_name
+  fi
 fi
