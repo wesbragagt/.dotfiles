3060a8fc3273a3eaf9aa708c7982a12fdba361b8
Weslley Braga <wesbragagt@gmail.com>
2022-07-31 10:19:35 -0500

    add explorer

--- nvim/.config/nvim/lua/wesbragagt/explorer.lua
+++ nvim/.config/nvim/lua/wesbragagt/explorer.lua
@@ -1,7 +1,7 @@
 -- Autorefresh when focusing
 vim.cmd([[
 autocmd BufEnter NERD_tree_* | execute 'normal R'
-let g:NERDTreeWinSize=80
+let g:NERDTreeWinSize=30
 let g:NERDTreeWinPos = "right"
 let NERDTreeShowHidden=1
 let mapleader = ' '
