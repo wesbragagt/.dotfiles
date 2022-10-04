d35d67d870b7a36c369457e7100f8b3f73c612b7
wesbragagt <weslley@bambee.com>
2022-10-04 01:41:39 -0500

    harpoon maaan

--- nvim/.config/nvim/lua/wesbragagt/plugins.lua
+++ nvim/.config/nvim/lua/wesbragagt/plugins.lua
@@ -103,6 +103,7 @@ return packer.startup(function(use)
 	use("rcarriga/nvim-dap-ui")
 	use("theHamsta/nvim-dap-virtual-text")
 	use("gabrielpoca/replacer.nvim")
+	use("ThePrimeagen/harpoon")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
