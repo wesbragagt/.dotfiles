c5ee365c3ff238d19d1094369d05efaa284e5547
wesbragagt <wesbragagt@gmail.com>
2022-10-09 15:55:37 -0500

    add new countdown script

--- utils/countdown.sh
+++ utils/countdown.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if ! command -v toilet &> /dev/null; then
+  echo "Need toilet for this script to work"
+  echo "brew install toilet"
+  exit 1
+fi
+
 # Displays a countdown with ascii text art
  hour=$1
  min=$2
  sec=$3
-
  while [ $hour -ge 0 ]; do
    while [ $min -ge 0 ]; do
      while [ $sec -ge 0 ]; do
+       clear
        toilet -f smmono9 -w 1000 "Starting in $hour:$min:$sec"
        let "sec=sec-1"
        sleep 1
