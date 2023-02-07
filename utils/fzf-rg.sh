739faf17562f3da912f8535915ff6e0e4c0417b0
wesbragagt <wesbragagt@gmail.com>
2023-02-07 00:27:22 -0600

    new sg command to live grep with fzf

--- utils/fzf-rg.sh
+++ utils/fzf-rg.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# pipes ripgrep search to fzf selection and opens vim on selected file
-set -e
-
 # allows selecting files
 INITIAL_QUERY=""
-RG_PREFIX="rg --files-with-matches"
+RG_PREFIX="rg --column --color=always --smart-case "
 FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'"
 
 fzf_output=$(fzf -m --bind "ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all,change:reload:$RG_PREFIX {q} || true" \
-      --print-query \
       --query "$INITIAL_QUERY" \
-      --preview 'bat --style=numbers --color=always --line-range :500 {}')
+      --ansi --disabled
+    )
 
 if [[ -n "$fzf_output" ]]; then
-  # Get the searched word from output
-  final_query=$(echo $fzf_output | head -n1 | awk '{print $1;}')
-
-  nvim -q <(rg --column $final_query) +cw
+  nvim -q <(printf "$fzf_output") +cw
 fi
 
 if [[ $? -eq 0 ]]; then
