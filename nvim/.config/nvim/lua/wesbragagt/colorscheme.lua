d595eb4f347fb11bb2ec1a9241e045eafa43257d
Wes Braga <wesbragagt@gmail.com>
2023-06-12 15:54:22 -0500

    fix: turn off semantic highlighting

--- nvim/.config/nvim/lua/wesbragagt/colorscheme.lua
+++ nvim/.config/nvim/lua/wesbragagt/colorscheme.lua
@@ -8,3 +8,8 @@ vim.cmd([[
     colorscheme default
   endtry
 ]])
+
+-- Hide all semantic highlights
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  vim.api.nvim_set_hl(0, group, {})
+end
