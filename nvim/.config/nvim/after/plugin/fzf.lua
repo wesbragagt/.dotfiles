325a47f5ed8859e6c27653c19ebc59af53785c67
Weslley <wesbragagt@gmail.com>
2023-06-24 15:06:47 -0500

    fzf-lua fd

--- nvim/.config/nvim/after/plugin/fzf.lua
+++ nvim/.config/nvim/after/plugin/fzf.lua
@@ -8,11 +8,14 @@ local my_utils = require("utils")
 
 -- Falling back to find_files if git_files can't find a .git directory
 local function project_files()
-	if require("utils").is_git_repo() then
-		fzf.git_files({ cmd = 'git ls-files --others --exclude="node_modules/"' })
-	else
-		fzf.find_files()
-	end
+   local cwd = my_utils.get_git_root_with_fallback()
+  local excluded_folders = { 'node_modules', '.git' }
+  local excluded = ''
+  for _, value in pairs(excluded_folders) do
+     excluded = excluded .. ' --exclude' .. ' ' .. value
+  end
+  local cmd = string.format('fd --type file -H %s', excluded)
+		fzf.files({ cmd, cwd = my_utils.get_git_root_with_fallback() })
 end
 
 local function live_grep()
