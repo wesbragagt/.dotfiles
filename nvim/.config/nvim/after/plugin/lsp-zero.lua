44ee0214dda6e128d0ef5fa3af6a9ff06545a564
wesbragagt <wesbragagt@gmail.com>
2023-10-28 16:45:53 -0500

    golang setup pretty cool

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -47,6 +47,19 @@ lsp.ensure_installed(
     "pyright"
   }
 )
+
+require("go").setup({})
+-- format on save golang
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+
 lsp.configure('lua_ls', {
   settings = {
     Lua = {
--- nvim/.config/nvim/lua/wesbragagt/plugins.lua
+++ nvim/.config/nvim/lua/wesbragagt/plugins.lua
@@ -153,6 +153,7 @@ return packer.startup(function(use)
   use({ "nvim-pack/nvim-spectre" })
   use({ "windwp/nvim-autopairs" })
   use({ "github/copilot.vim" })
+  use({"ray-x/go.nvim"})
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
