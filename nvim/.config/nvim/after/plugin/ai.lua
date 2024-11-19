1a853f82d3c8e417ca3aa354e573229cc39677cd
Weslley <wesbragagt@gmail.com>
2024-11-18 22:45:53 -0600

    chore: add snacks

--- nvim/.config/nvim/after/plugin/ai.lua
+++ nvim/.config/nvim/after/plugin/ai.lua
@@ -14,6 +14,22 @@ copilot_chat.setup({
       ]],
       description = "Custom prompt for creating unit tests in python using pytest.",
       selection = require('CopilotChat.select').visual
+    },
+    JestTest = {
+      prompt = [[
+        As a Jest expert, write a comprehensive unit test for the following program, aiming to maximize code coverage and ensure thorough testing of edge cases and expected behaviors. Mock dependencies where needed and do not assert unnecessary calls such as loggers. When creating the test context, consider pulling it out to a separate function called testContext where the spies, service and mocks are created. For example:
+
+        function testContext(){
+          const mockRepo = createMock<Repo>()
+          const service = new Service(mockRepo)
+          const spies = {
+            repo: jest.spyOn(mockRepo, 'method')
+          }
+          return {service, spies}
+        }
+      ]],
+      description = "Custom prompt for creating unit tests in javascript using jest.",
+      selection = require('CopilotChat.select').visual
     }
   }
 })
