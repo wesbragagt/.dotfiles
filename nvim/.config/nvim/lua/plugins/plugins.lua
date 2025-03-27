5336c27d91ffaf702c6d0a5ea99963fa2885ba20
Weslley <wesbragagt@gmail.com>
2025-03-26 22:21:11 -0500

    chore: clean up unused plugins

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -159,7 +159,6 @@ return {
     event = "VeryLazy"
   },
   { "theHamsta/nvim-dap-virtual-text" },
-  { "SSHari/jest.nvim" },
   { "stevearc/oil.nvim" },
   {
     "iamcco/markdown-preview.nvim",
@@ -167,60 +166,10 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
-  {
-    "epwalsh/obsidian.nvim",
-    tag = "v3.9.0", -- recommended, use latest release instead of latest commit
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
   { "windwp/nvim-autopairs",  event = "InsertEnter" },
   { "nvim-pack/nvim-spectre", event = "VeryLazy" },
   {
     "github/copilot.vim"
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
   },
   { 'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons', opts = {} },
   {
@@ -228,8 +177,5 @@ return {
     version = "*",
     config = true,
     event = "VeryLazy"
-  },
-  {
-    "tpope/vim-dispatch"
   }
 }
