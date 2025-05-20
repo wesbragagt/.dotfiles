46c26d55c7eeb07b895c5d215bb5b76fe2ac2216
Weslley <wesbragagt@gmail.com>
2025-05-20 10:04:16 -0500

    chore: update plugins

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -240,6 +240,7 @@ require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 require("mason-lspconfig").setup({
 	ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
 	automatic_installation = false,
+  automatic_enable = true,
 	handlers = {
 		function(server_name)
 			local server = servers[server_name] or {}
