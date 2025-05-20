09bd2f8259200d111197bbc7098feff3c141b5aa
Weslley <wesbragagt@gmail.com>
2025-05-20 16:48:20 -0500

    feat: modify the map commands in lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -42,40 +42,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		-- Rename the variable under your cursor.
 		--  Most Language Servers support renaming across files, etc.
-		map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>rb", vim.lsp.buf.rename, "[R]e[n]ame")
 
-		-- Execute a code action, usually your cursor needs to be on top of an error
-		-- or a suggestion from your LSP for this to activate.
-		map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
-		-- Find references for the word under your cursor.
-		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("gr", vim.lsp.buf.references, "[G]o to [R]eferences")
+    map("gd", vim.lsp.buf.definition, "[G]o to [D]efinition")
 
-		-- Jump to the implementation of the word under your cursor.
-		--  Useful when your language has ways of declaring types without an actual implementation.
-		map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-		-- Jump to the definition of the word under your cursor.
-		--  This is where a variable was first declared, or where a function is defined, etc.
-		--  To jump back, press <C-t>.
-		map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-		-- WARN: This is not Goto Definition, this is Goto Declaration.
-		--  For example, in C this would take you to the header.
-		map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-		-- Fuzzy find all the symbols in your current document.
-		--  Symbols are things like variables, functions, types, etc.
-		map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-
-		-- Fuzzy find all the symbols in your current workspace.
-		--  Similar to document symbols, except searches over your entire project.
-		map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-
-		-- Jump to the type of the word under your cursor.
-		--  Useful when you're not sure what type a variable is and you want to see
-		--  the definition of its *type*, not where it was *defined*.
-		map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+    map("L", vim.diagnostic.open_float, "Shows the diagnostics in a floating window")
+    map("K", vim.lsp.buf.hover, "Shows the hover information in a floating window.")
+    map("<leader>f", vim.lsp.buf.format)
 
 		-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 		---@param client vim.lsp.Client
@@ -290,7 +266,7 @@ require("blink.cmp").setup({
 			completion = {
 				-- By default, you may press `<c-space>` to show the documentation.
 				-- Optionally, set `auto_show = true` to show the documentation after a delay.
-				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+				documentation = { auto_show = true, auto_show_delay_ms = 100 },
 				menu = { border = "single" },
 			},
 
