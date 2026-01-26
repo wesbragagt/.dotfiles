9a9cf3e5473804f3f773450dac532867ac6c4966
Weslley <wesbragagt@gmail.com>
2026-01-26 15:30:45 -0600

    fix: add -L flag to follow symlinks in wallpaper script

--- utils/random-wallpaper.sh
+++ utils/random-wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 WALLPAPERS_DIR="$HOME/wallpapers"
-WALLPAPER=$(find "$WALLPAPERS_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+WALLPAPER=$(find -L "$WALLPAPERS_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
 swww img "$WALLPAPER" --transition-type wipe --transition-duration 2
