7d8980ee67d71907d8d4c21f5deb07d55750f997
Weslley <wesbragagt@gmail.com>
2026-01-26 15:32:29 -0600

    fix: update correct wallpaper script in modules/utils

--- nixos/modules/utils/random-wallpaper.sh
+++ nixos/modules/utils/random-wallpaper.sh
@@ -1,5 +1,5 @@
-    #!/usr/bin/bash
+#!/usr/bin/bash
 
-    WALLPAPERS_DIR=~/wallpapers/anime-original-freedom-flight.jpg # Adjust this path as needed
-    WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1) # Randomly select an image
-    swww img "$WALLPAPER" --transition-type wipe --transition-duration 2
+WALLPAPERS_DIR="$HOME/wallpapers"
+WALLPAPER=$(find -L "$WALLPAPERS_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+swww img "$WALLPAPER" --transition-type wipe --transition-duration 2
