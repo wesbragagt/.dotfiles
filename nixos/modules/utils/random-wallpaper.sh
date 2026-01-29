d6f3be0644fa5a3439b37bc5ea38598ec0d98d69
Weslley <wesbragagt@gmail.com>
2026-01-28 18:45:03 -0600

    fix: add WAYLAND_DISPLAY fallback to random-wallpaper script for systemd compatibility

--- nixos/modules/utils/random-wallpaper.sh
+++ nixos/modules/utils/random-wallpaper.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-wayland-1}
+
 WALLPAPERS_DIR="$HOME/wallpapers"
 WALLPAPER=$(find -L "$WALLPAPERS_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
 swww img "$WALLPAPER" --transition-type wipe --transition-duration 2
