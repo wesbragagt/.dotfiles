0b1462d58a37c470a67a21713390344c4b9eabb6
Weslley <wesbragagt@gmail.com>
2026-02-11 22:37:59 -0600

    fix: correct theme path in rofi clipboard script

--- nixos/modules/utils/rofi-clipboard.sh
+++ nixos/modules/utils/rofi-clipboard.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cliphist list | rofi -dmenu -theme ~/.config/rofi/raycast-nord.rasi | cliphist decode | wl-copy
+cliphist list | rofi -dmenu -theme ~/.config/rofi/themes/raycast-nord.rasi | cliphist decode | wl-copy
